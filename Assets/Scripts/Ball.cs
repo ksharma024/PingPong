d5f43f19957e438b3bbb32828536c1b641edbb8f
Karn Sharma <karn.sharma024@gmail.com>
2016-07-17 21:47:47 -0400

    Final Commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public Vector3 initialImpulse;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().AddForce(initialImpulse, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/RightCapsule.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightCapsule : MonoBehaviour {
+
+    public enum ePlayer
+    {
+        Left,
+        Right
+    }
+
+    public float speed = 15f;
+    public ePlayer player;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float inputSpeed = 0f;
+        if (player == ePlayer.Left)
+        {
+            inputSpeed = Input.GetAxisRaw("LeftCapsule");
+        }
+
+        else if (player == ePlayer.Right)
+        {
+            inputSpeed = Input.GetAxisRaw("RightCapsule");
+             }
+
+        transform.position += new Vector3(0f, 0f, inputSpeed * speed * Time.deltaTime);
+	}
+}
